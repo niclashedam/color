f7cb34939f6343123469f12128bb471869ea9382
Niclas Hedam <niclas@frax.dk>
2017-01-31 13:45:44 +0000

    Apply fixes from StyleCI

--- tests/ColorTest.php
+++ tests/ColorTest.php
@@ -6,26 +6,27 @@ use NiclasHedam\Color;
 
 class ColorTest extends \PHPUnit_Framework_TestCase
 {
-    var $color1;
-    var $color2;
-    var $color3;
-    var $color4;
+    public $color1;
+    public $color2;
+    public $color3;
+    public $color4;
 
-    var $color5;
-    var $color6;
+    public $color5;
+    public $color6;
 
     public function __construct()
     {
-        $this->color1 = Color::fromRGB(0,0,0); //Black
-        $this->color2 = Color::fromRGB(255,255,255); //White
-        $this->color3 = Color::fromRGB(255,153,102); //Atomic Tangerine
-        $this->color4 = Color::fromRGB(102,255,255); //Light blue
+        $this->color1 = Color::fromRGB(0, 0, 0); //Black
+        $this->color2 = Color::fromRGB(255, 255, 255); //White
+        $this->color3 = Color::fromRGB(255, 153, 102); //Atomic Tangerine
+        $this->color4 = Color::fromRGB(102, 255, 255); //Light blue
 
         $this->color5 = Color::fromCMYK(47, 48, 0, 68);
         $this->color6 = Color::fromCMYK(0, 85, 85, 49);
     }
 
-    public function testLikeliness(){
+    public function testLikeliness()
+    {
         $this->assertEquals($this->color1->differenceBetween($this->color2), 100); //100
         $this->assertEquals($this->color2->differenceBetween($this->color1), 100); //100
         $this->assertEquals($this->color3->differenceBetween($this->color2), 41.6333);
@@ -33,40 +34,41 @@ class ColorTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($this->color3->differenceBetween($this->color4), 54.1603);
     }
 
-    public function testName(){
-        $this->assertEquals($this->color1->name(), "Black");
-        $this->assertEquals($this->color2->name(), "White");
-        $this->assertEquals($this->color3->name(), "Atomic Tangerine");
+    public function testName()
+    {
+        $this->assertEquals($this->color1->name(), 'Black');
+        $this->assertEquals($this->color2->name(), 'White');
+        $this->assertEquals($this->color3->name(), 'Atomic Tangerine');
         $this->assertEquals($this->color4->name(), false);
     }
 
-    public function testConversion(){
-        $this->assertEquals($this->color5->toRGB(), array('r' => 43, 'g' => 42, 'b' => 82));
-        $this->assertEquals($this->color6->toRGB(), array('r' => 130, 'g' => 20, 'b' => 20));
+    public function testConversion()
+    {
+        $this->assertEquals($this->color5->toRGB(), ['r' => 43, 'g' => 42, 'b' => 82]);
+        $this->assertEquals($this->color6->toRGB(), ['r' => 130, 'g' => 20, 'b' => 20]);
 
-        $this->assertEquals($this->color1->toHEX(), "#000000");
-        $this->assertEquals($this->color2->toHEX(), "#FFFFFF");
+        $this->assertEquals($this->color1->toHEX(), '#000000');
+        $this->assertEquals($this->color2->toHEX(), '#FFFFFF');
     }
 
-    public function testInvalids(){
-
-        try{
+    public function testInvalids()
+    {
+        try {
             Color::fromRGB(215, 242, 275);
             $this->fail();
-        }catch(Exception $e){
+        } catch (Exception $e) {
         }
 
-        try{
+        try {
             Color::fromRGB(-252, 242, 35);
             $this->fail();
-        }catch(Exception $e){
+        } catch (Exception $e) {
         }
 
-        try{
+        try {
             $color = Color::fromCMYK(105, 45, 23, 66);
             $this->fail();
-        }catch(Exception $e){
+        } catch (Exception $e) {
         }
-
     }
 }
